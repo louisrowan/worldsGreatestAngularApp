924b1dde3a56929836ca8022f9eb59203dae4f0f
Louis Rowan <louis.rowan@icloud.com>
2017-02-03 01:23:46 -0500

    creates factory

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 var MyApp = angular.module('MyApp', []);
 
-MyApp.controller('MainCtrl', function($scope){
-    console.log($scope)
-    $scope.world = 'Front End Masters'
-  })
+MyApp.controller('MainCtrl', function($scope, data){
+  $scope.doStuff = data.getStuff
+  $scope.func = data.func()
+  console.log($scope.doStuff)
+  console.log($scope.func)
+  $scope.world = 'Front End Masters'
+})
 
 MyApp.controller('AltCtrl', function($scope){
-  $scope.world = 'bananas'
+  $scope.a = 'bananas'
+  $scope.b = 'apples'
+  $scope.c = $scope.a
+})
+
+MyApp.factory('data', function(){
+  var things = [
+    {name: "bob", age: 14},
+    {name: "mary", age: 20}
+  ]
+
+  var func = function(){
+    return things
+  }
+
+  return {
+    getStuff: things,
+    func: func
+  }
 })
