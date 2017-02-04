445e8c2a14377858392289f5881d98e801956e82
Louis Rowan <louis.rowan@icloud.com>
2017-02-03 22:13:02 -0500

    beings to add logic to calc

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -4,9 +4,44 @@ CalcApp.controller('CalcCtrl', function($scope, buttonsFact){
   $scope.buttons = buttonsFact.buttons
 
   $scope.value = ''
+  var firstNum = ''
+  var operation = ''
+  var current = ''
 
   $scope.buttonClick = function(index){
-    $scope.value += $scope.buttons[index].val
+    var buttonVal = $scope.buttons[index].val
+    console.log('scope=', $scope.value)
+    console.log('firstNum=', firstNum)
+    console.log('operation=', operation)
+
+    if (buttonVal === 'C') {
+      $scope.value = ''
+      firstNum = ''
+      operation = ''
+      current = ''
+      return;
+    }
+    else if (buttonVal === '=') {
+      console.log(firstNum, operation, $scope.value)
+    }
+    else if (typeof(buttonVal) === 'number') {
+      if (operation === '') {
+        firstNum += buttonVal
+      }
+      else if (firstNum === $scope.value){
+        $scope.value = buttonVal
+        return
+      }
+        $scope.value += buttonVal
+        return;
+    }
+    else {
+      if ($scope.value === operation){
+        $scope.value = 'error'
+      } else {
+        firstNum = $scope.value
+      }
+    }
   }
 })
 
