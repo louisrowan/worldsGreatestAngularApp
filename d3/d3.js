df5882b5d8521690719d30ace130e2f34f2211a1
Louis Rowan <louis.rowan@icloud.com>
2017-02-05 17:38:47 -0500

    experimenting with d3 race idea

--- d3/d3.js
+++ d3/d3.js
@@ -1,5 +1,29 @@
+var app = angular.module('d3App', [])
+
+app.controller('d3Controller', function($scope){
+
+})
+
+var circleData = [
+  {name: 'A', speed: 5},
+  {name: 'B', speed: 10},
+  {name: 'C', speed: 15}
+]
+
+
 var svg = d3.select('#svgDiv')
   .append('svg')
   .attr('height', '100%')
   .attr('width', '100%')
-  .style('border', '2px solid green')
+  .attr('position', 'absolute')
+
+var g = svg.selectAll('g')
+  .data(circleData)
+  .enter()
+  .append('g')
+
+var circles = g.append('circle')
+  .attr('r', 20)
+  .style('fill', 'red')
+  .attr('cx', 100)
+  .attr('cy', (d, i) => 100*i + 100)
